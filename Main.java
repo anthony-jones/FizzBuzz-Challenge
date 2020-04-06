d1c6398f8badd4af2d0ae90ee18f2a09d2688a56
Anthony Jones <acejones81@gmail.com>
2020-04-06 12:58:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void fizzBuzz(){
         for( int i = 1; i <= 100; i++){
             if(i % 15 == 0){
-                System.out.println("Fizz Buzz");
+                System.out.println("FizzBuzz");
             } else if (i % 5 == 0){
                 System.out.println("Buzz");
             } else if ( i % 3 == 0){
